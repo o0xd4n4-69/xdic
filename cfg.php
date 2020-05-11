ac0280bd6a3271847ef4b4920574650d41adac9d
o0xd4n4-69 <65133233+o0xd4n4-69@users.noreply.github.com>
2020-05-11 14:53:58 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+//masukan sesuai dibawah
+$cookie4 = "cookie: __cfduid=d66864ec8fc08e1e904983e7aee29e3f21587250312";
+$cookie5 = "cookie: ExpressCrypto=cc5959f20d44e52a3df7e243a5c75d39";
+$cookie6 = "cookie: _ga=GA1.1.1875024199.1584648711";
+$cookie8 = "cookie: __gads=ID=925b477a4a9711e0:T=1585874525:S=ALNI_Mbj1J1D4DGjzppoHhpZfBk-fo49IQ";
+$cookie9 = "cookie: _ga_8ES1N2JB0Q=GS1.1.1589106004.58.0.1589106004.0";
+$clientseedbtc = "pgzij1shrvwyxbgvbmt";
+$clientseedltc = "hvcr2od5nbk3gfc7rkee6";
+$clientseedoge = "f5lhuueva4n6f2g0v";
+
+
+?>
+
+
